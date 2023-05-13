fde7deed8de93380f655f482c38b2fd97d07ba25
rwiteshbera <rwiteshbera@gmail.com>
2023-05-13 23:59:15 +0530

    minor changes

--- EventCatalogService/controllers/event.go
+++ EventCatalogService/controllers/event.go
@@ -1,28 +1,29 @@
 package controllers
 
 import (
+	"database/sql"
 	"eventCatalogService/api"
-	"fmt"
+	"eventCatalogService/database"
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 	"net/http"
 )
 
 func CreateEvent(server *api.Server) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-			server.Config.POSTGRES_HOST, server.Config.POSTGRES_USER, server.Config.POSTGRES_PASSWORD, server.Config.POSTGRES_DB,
-			server.Config.POSTGRES_PORT)
-		_, err := gorm.Open(postgres.New(postgres.Config{
-			DSN: dsn, PreferSimpleProtocol: true,
-		}), &gorm.Config{})
-
+		db, err := database.ConnectPostgres(server)
+		d, _ := db.DB()
+		defer func(d *sql.DB) {
+			err := d.Close()
+			if err != nil {
+				LogError(ctx, http.StatusInternalServerError, err)
+				return
+			}
+		}(d)
+		
 		if err != nil {
 			LogError(ctx, http.StatusInternalServerError, err)
 			return
 		}
-
 		ctx.JSON(http.StatusOK, gin.H{"message": "success"})
 	}
 }
